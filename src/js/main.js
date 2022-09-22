03d18fb7ed505a0253331687029bc011ef0e93f3
Dima Hrachev <babun5678@gmail.com>
2022-09-23 00:55:34 +0300

    add gsap

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
 // import "../../node_modules/focus-visible/dist/focus-visible";
 
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import ScrollSmoother from "../js/ScrollSmoother";
+
 import "../scss/main.scss";
 import "../index.html";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollSmoother.create({
+  smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+  effects: true, // looks for data-speed and data-lag attributes on elements
+  smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
