94b996e24a70a60bbf2c5bfcf63a1672710e3b2c
Ivan-Shalnov <98489312+Ivan-Shalnov@users.noreply.github.com>
2022-09-23 02:28:43 +0200

    promo section

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,47 @@ import "../index.html";
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 ScrollSmoother.create({
-  smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+  smooth: 2, // how long (in seconds) it takes to "catch up" to the native scroll position
   effects: true, // looks for data-speed and data-lag attributes on elements
   smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
+window.addEventListener("DOMContentLoaded", () => {
+  // PROMO SECTION
+  {
+    const wrapRef = document.querySelector(".promo__wrap");
+    const imgsRefs = wrapRef.querySelectorAll(".promo__img");
+    const getScrollLength = () =>
+      wrapRef.scrollWidth - document.body.clientWidth;
+    const promoTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".promo",
+        start: "center center",
+        end: () => getScrollLength() + "px",
+        scrub: 1,
+        pin: true,
+        invalidateOnRefresh: true,
+        anticipatePin: 1,
+      },
+    });
+    ease: "none",
+      promoTl.to(".promo__wrap", {
+        x: () => -getScrollLength(),
+        ease: "none",
+      });
+    imgsRefs.forEach((el) => {
+      gsap.to(el, {
+        xPercent: -40,
+        ease: "none",
+        scrollTrigger: {
+          trigger: el,
+          containerAnimation: promoTl,
+          start: "left right",
+          scrub: true,
+          markers: true,
+          horizontal: true,
+        },
+      });
+    });
+  }
+  // PROMO SECTION
 });
